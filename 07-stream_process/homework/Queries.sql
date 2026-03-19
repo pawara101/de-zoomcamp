0bcbf78d8edfe89199a229a043e17ec77f070160
Pawara Tharkana <pawara101dassanayake@gmail.com>
2026-03-19 23:48:31 +0530

    Week07 homework

---
+++ 07-stream_process/homework/Queries.sql
@@ -0,0 +1,10 @@
+CREATE TABLE processed_events (
+    PULocationID INTEGER,
+    DOLocationID INTEGER,
+    trip_distance DOUBLE PRECISION,
+    total_amount DOUBLE PRECISION,
+    tip_amount DOUBLE PRECISION,
+    pickup_datetime TIMESTAMP,
+    dropoff_datetime TIMESTAMP,
+    passenger_count INTEGER
+);
---
+++ 07-stream_process/homework/src/job/pass_through_job.py
@@ -0,0 +1,87 @@
+from pyflink.datastream import StreamExecutionEnvironment
+from pyflink.table import EnvironmentSettings, StreamTableEnvironment
+
+def create_events_source_kafka(t_env):
+    table_name = "events"
+    source_ddl = f"""
+        CREATE TABLE {table_name} (
+    PULocationID INTEGER,
+    DOLocationID INTEGER,
+    trip_distance DOUBLE PRECISION,
+    total_amount DOUBLE PRECISION,
+    tip_amount DOUBLE PRECISION,
+    pickup_datetime TIMESTAMP,
+    dropoff_datetime TIMESTAMP,
+    passenger_count INTEGER
+        ) WITH (
+            'connector' = 'kafka',
+            'properties.bootstrap.servers' = 'redpanda:29092',
+            'topic' = 'rides',
+            'scan.startup.mode' = 'latest-offset',
+            'properties.auto.offset.reset' = 'latest',
+            'format' = 'json'
+        );
+        """
+    t_env.execute_sql(source_ddl)
+    return table_name
+
+
+def create_processed_events_sink_postgres(t_env):
+    table_name = 'processed_events'
+    sink_ddl = f"""
+        CREATE TABLE {table_name} (
+    PULocationID INTEGER,
+    DOLocationID INTEGER,
+    trip_distance DOUBLE PRECISION,
+    total_amount DOUBLE PRECISION,
+    tip_amount DOUBLE PRECISION,
+    pickup_datetime TIMESTAMP,
+    dropoff_datetime TIMESTAMP,
+    passenger_count INTEGER
+        ) WITH (
+            'connector' = 'jdbc',
+            'url' = 'jdbc:postgresql://postgres:5432/postgres',
+            'table-name' = '{table_name}',
+            'username' = 'postgres',
+            'password' = 'postgres',
+            'driver' = 'org.postgresql.Driver'
+        );
+        """
+    t_env.execute_sql(sink_ddl)
+    return table_name
+
+def log_processing():
+    # Set up the execution environment
+    env = StreamExecutionEnvironment.get_execution_environment()
+    env.enable_checkpointing(10 * 1000)
+
+    # Set up the table environment
+    settings = EnvironmentSettings.new_instance().in_streaming_mode().build()
+    t_env = StreamTableEnvironment.create(env, environment_settings=settings)
+    try:
+        # Create Kafka table
+        source_table = create_events_source_kafka(t_env)
+        postgres_sink = create_processed_events_sink_postgres(t_env)
+        # write records to postgres
+        t_env.execute_sql(
+            f"""
+                    INSERT INTO {postgres_sink}
+                    SELECT
+                        PULocationID,
+                        DOLocationID,
+                        trip_distance,
+                        total_amount,
+                        tip_amount,
+                        TO_TIMESTAMP_LTZ(lpep_pickup_datetime, 3) as pickup_datetime,
+                        TO_TIMESTAMP_LTZ(lpep_dropoff_datetime, 3) as dropoff_datetime,
+                        passenger_count
+                    FROM {source_table}
+                    """
+        ).wait()
+
+    except Exception as e:
+        print("Writing records from Kafka to JDBC failed:", str(e))
+
+
+if __name__ == '__main__':
+    log_processing()
